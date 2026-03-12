a9e30acf5254cffaa8db375ac2bda9a6248c5e44
Ria Dutta <riadutta257@gmail.com>
2026-03-12 10:43:25 +0530

    Added project files

--- src/com/sum/demo.java
+++ src/com/sum/demo.java
@@ -5,7 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10;
-		int b=20;
+		int b=50;
 		int c=a+b;
 		System.out.println("The sum of a and b is: "+c);
 	}
