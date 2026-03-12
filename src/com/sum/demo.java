8d63cb6ce59f50b63479c5d6e0fec1294449873c
Ria Dutta <riadutta257@gmail.com>
2026-03-12 10:09:40 +0530

    Comments

---
+++ src/com/sum/demo.java
@@ -0,0 +1,13 @@
+package com.sum;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("The sum of a and b is: "+c);
+	}
+
+}
